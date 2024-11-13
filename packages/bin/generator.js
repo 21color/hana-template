54b24768e3c7adc12392c86062f899ab7b112423
21color <lehhha0110@gmail.com>
2024-11-13 14:17:08 +0900

    feat: package.json 수정

---
+++ packages/bin/generator.js
@@ -0,0 +1,72 @@
+#! /usr/bin/env node
+const { execSync } = require("child_process");
+const path = require("path");
+const fs = require("fs");
+const { removeSync, copySync } = require("fs-extra");
+
+// project-name 미입력
+if (process.argv.length < 3) {
+  console.log("[ERROR]: Enter as in the example below");
+  console.log("ex) npx vite-create-21color [project-name]");
+  process.exit(1);
+}
+
+const projectName = process.argv[2];
+const currentPath = process.cwd();
+const isCurrentPathProject = projectName === ".";
+
+const tempPath = path.join(currentPath, "temp");
+const projectPath = isCurrentPathProject
+  ? currentPath
+  : path.join(currentPath, projectName);
+
+const GIT_REPO = "https://github.com/21color/21color-template.git";
+
+// project-name 입력시
+if (!isCurrentPathProject) {
+  try {
+    fs.mkdirSync(projectPath);
+  } catch (err) {
+    if (err.code === "EEXIST") {
+      // 이미 해당 경로 존재
+      console.log(
+        `[ERROR]: The file ${projectName} already exist in the current directory.`
+      );
+    } else {
+      console.log(error);
+    }
+    process.exit(1);
+  }
+}
+
+async function main() {
+  try {
+    // git clone
+    console.log("[INFO]: Downloading vite-create-21color...");
+    execSync(`git clone ${GIT_REPO} ${tempPath}`);
+
+    // 임시 폴더에서 template만 복사
+    console.log("[INFO]: Copying files...");
+    copySync(`${tempPath}/template`, projectPath);
+
+    // 임시 폴더 삭제
+    removeSync(tempPath);
+
+    // 현재 경로 이동
+    console.log("[INFO]: Moving into directory...");
+    if (!isCurrentPathProject) {
+      process.chdir(projectPath);
+    }
+
+    // 의존성 설치
+    console.log("[INFO]: install dependencies...");
+    execSync("npm install");
+
+    // SUCCESS !
+    console.log("[SUCCESS]: Success to vite-create-21color. Available now !");
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+main();
